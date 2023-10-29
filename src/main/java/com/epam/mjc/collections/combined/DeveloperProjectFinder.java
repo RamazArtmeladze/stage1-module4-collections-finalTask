d571d26847037f21f6bd70f6ed1ad7fb42e3f4e7
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-10-30 00:59:28 +0400

    Update

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,31 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> resultProjects = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            String projectName = entry.getKey();
+            Set<String> developers = entry.getValue();
+
+            if (developers.contains(developer)) {
+                resultProjects.add(projectName);
+            }
+        }
+
+        Collections.sort(resultProjects, new Comparator<String>() {
+            @Override
+            public int compare(String project1, String project2) {
+                if (project1.length() != project2.length()) {
+                    return Integer.compare(project2.length(), project1.length());
+                }
+                return project2.compareTo(project1);
+            }
+        });
+
+        return resultProjects;
     }
 }
+
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,23 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> resultMap = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            String key = entry.getKey();
+            int length = key.length();
+
+            if (!resultMap.containsKey(length)) {
+                resultMap.put(length, new HashSet<>());
+            }
+
+            resultMap.get(length).add(key);
+        }
+
+        return resultMap;
     }
 }
+
